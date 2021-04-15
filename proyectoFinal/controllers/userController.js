2514f748c6eb98af79f478a084866afe0da4501e
valenvinyo <valentinavinyolas02@gmail.com>
2021-04-15 19:46:50 -0300

    cambios en productos y formulario

--- proyectoFinal/controllers/userController.js
+++ proyectoFinal/controllers/userController.js
@@ -1,8 +1,9 @@
+let products = require("../data/productsData")
 
 let controller = {
     login:(req,res)=>   res.render('login', { title: 'Ingresa a The Union Winery' }),
     register:(req,res)=>   res.render('register', { title: 'Registrarse a The Union Winery' }),
-    profile:(req,res)=>   res.render('profile', { title: 'Tu perfil de The Union Winery' }),
+    profile:(req,res)=>   res.render('profile', { title: 'Tu perfil de The Union Winery', productos:products }),
     edit:(req,res)=> res.render('profile-edit', {title:'Edita Tu Perfil de The Union Winery'}),
     logout:(req,res)=>   res.render('logout', { title: 'Logout de The Union Winery' }) 
 
