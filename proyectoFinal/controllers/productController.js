42705e57a16bdde40c716db7b256c8874f39ec0b
valenvinyo <valentinavinyolas02@gmail.com>
2021-04-15 10:54:34 -0300

    cargados todos los vinos y cambios en detalles de estilo

--- proyectoFinal/controllers/productController.js
+++ proyectoFinal/controllers/productController.js
@@ -1,9 +1,9 @@
 let products = require('../data/productsData');
 
 let controller = {
-    show:(req,res)=>    res.render('product', { title: 'Productos Proyecto X' , products: products }),
-    edit:(req,res)=>    res.render('product-add', {title: 'Editar Productos Proyecto X', products: products}),
-    search:(req,res)=>   res.render('search-results', { title: 'Resultados Proyecto X' , result: req.params.search}) ,
+    show:(req,res)=>    res.render('product', { title: 'Productos de The Union Winery' , products: products }),
+    edit:(req,res)=>    res.render('product-add', {title: 'Editar Productos de The Union Winery', products: products}),
+    search:(req,res)=>   res.render('search-results', { title: 'Resultados de The Union Winery' , result: req.params.search}) ,
 
 } 
 
--- proyectoFinal/controllers/userController.js
+++ proyectoFinal/controllers/userController.js
@@ -1,10 +1,10 @@
 
 let controller = {
-    login:(req,res)=>   res.render('login', { title: 'Ingresa a Proyecto X' }),
-    register:(req,res)=>   res.render('register', { title: 'Registrarse a Proyecto X' }),
-    profile:(req,res)=>   res.render('profile', { title: 'Tu perfil de Proyecto X' }),
-    edit:(req,res)=> res.render('profile-edit', {title:'Edita Tu Perfil de Proyecto X'}),
-    logout:(req,res)=>   res.render('logout', { title: 'Logout' }) 
+    login:(req,res)=>   res.render('login', { title: 'Ingresa a The Union Winery' }),
+    register:(req,res)=>   res.render('register', { title: 'Registrarse a The Union Winery' }),
+    profile:(req,res)=>   res.render('profile', { title: 'Tu perfil de The Union Winery' }),
+    edit:(req,res)=> res.render('profile-edit', {title:'Edita Tu Perfil de The Union Winery'}),
+    logout:(req,res)=>   res.render('logout', { title: 'Logout de The Union Winery' }) 
 
 } 
 
