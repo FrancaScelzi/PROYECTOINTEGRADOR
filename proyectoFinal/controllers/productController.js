a8b916798d6c45207c9af225fdd6b9a0afb490f5
MiltonKienigiel <miltonkienigiel@gmail.com>
2021-04-15 19:14:32 -0300

    Detalle producto

--- proyectoFinal/controllers/productController.js
+++ proyectoFinal/controllers/productController.js
@@ -1,7 +1,7 @@
 let products = require('../data/productsData');
 
 let controller = {
-    show:(req,res)=>    res.render('product', { title: 'Productos de The Union Winery' , products: products }),
+    show:(req,res)=>    res.render('product', { title: 'Productos de The Union Winery' , products: products , id: req.params.id}),
     edit:(req,res)=>    res.render('product-add', {title: 'Editar Productos de The Union Winery', products: products}),
     search:(req,res)=>   res.render('search-results', { title: 'Resultados de The Union Winery' , result: req.params.search}) ,
 
--- proyectoFinal/data/productsData.js
+++ proyectoFinal/data/productsData.js
@@ -6,7 +6,7 @@ let vinos = [
         description:'Luigi Bosca Malbec es un tinto emblemático de la bodega, elaborado con uvas de viñedos propios situados en Luján de Cuyo y Valle de Uco, Mendoza. Tiene un color rojo violáceo profundo y brillante. Sus aromas son intensos y equilibrados, con notas de frutas rojas algo maduras y especias. En boca es generoso, fluido y expresivo, con taninos incipientes. De paladar franco y paso refrescante, con buen cuerpo y carácter atractivo. Su final es profundo y nítido, con el roble totalmente integrado, y posee un buen potencial de guarda. Es un vino insignia de su variedad.',
         variety: "Malbec",
         year: 2021,
-        img:"images/products/img-luigi-bosca-malbec.jpg"
+        img:"/images/products/img-luigi-bosca-malbec.jpg"
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ let vinos = [
         description:'Este tinto de color rojo violáceo muy profundo. Los intensos aromas frutados se presentan entremezclados con otros propios de la crianza en roble nuevo (vainilla, ahumados, tostados). En boca, su estructura concentrada deja la evocación de sus taninos suaves y sucrosos. Cosechado manualmente. Crianza en barril 12 meses. Tipo de roble 80% francés, nuevo; 20% americano, nuevo. Apto para guarda de unos 6 años. Decantar por espacio de una hora para su consumo. La temperatura adecuada para disfrutar este vino es entre 16º y 18º C. Este Malbec es ideal para combinarlo con carnes vacunas grilladas y horneadas, aves asadas, pastas con salsas cárnicas, pescados de río.',
         variety: "Malbec",
         year: 2021,
-        img:"images/products/img-rutini-colección-malbec.jpg"
+        img:"/images/products/img-rutini-colección-malbec.jpg"
 
     },
     {
@@ -25,7 +25,7 @@ let vinos = [
         description:'Varietales provenientes de las fincas que se ubican entre los 1050 y 1700 metros, con suelos pedregosos, gran amplitud térmica, optima exposición solar y agua pura de deshielo de los Andes que nos dan las condiciones ideales para elaborar vinos que son la máxima expresión del Valle de Uco. Este es de color rojo violáceo profundo de gran intensidad. En la nariz se percibe toda la tipicidad que presenta este varietal en el Valle de Uco, donde se destacan las bayas negras maduras, arándonos y grosellas. Se hacen presentes las delicadas notas florales de violetas. En boca presenta una entrada sedosa y envolvente. Amplio e intenso. Destacamos su acidez fresca y equilibrada. Es un vino franco donde las notas frutales se combinan con las notas aportadas por el paso por madera, especialmente las notas a vainilla. De largo y persistente final.',
         variety: "Malbec",
         year: 2014,
-        img:"images/products/img-salentein-reserve-malbec.jpg"
+        img:"/images/products/img-salentein-reserve-malbec.jpg"
 
     },
     {
@@ -35,7 +35,7 @@ let vinos = [
         description:'Bressia Monteagrelo fusiona el alma de cada varietal, su íntima relación con el origen y su profundo arraigo con un terroir único en el mundo: agrelo. Estos vinos son quilibrados pero intensos y seductores, cada uno de ellos impacta por su personalidad y estilo varietal. Presenta un color rojo intenso con tonalidades púrpuras propias del varietal. Perfecto equilibrio entre fruta y madera, muy pimentoso y especiado. Muy buen cuerpo, carnoso, con final de boca balanceado. Sin aristas, maduro y armónico.',
         variety: "Cabernet Sauvignon",
         year: 2009,
-        img:"images/products/img-bressia-monteagrelo-cabernet-sauvignon.jpg"
+        img:"/images/products/img-bressia-monteagrelo-cabernet-sauvignon.jpg"
 
     },
     {
@@ -45,7 +45,7 @@ let vinos = [
         description:'Color amarillo verdoso de buena intensidad y gran luminosidad Delicados aromas a durazno blanco, pomelo, ruda Muy fresco, frutal y cuerpo equilibrado. Muy armónico y de muy buena tipicidad varietal. Cosechado manualmente, tecnica de fermentación tradicional. No paso por barrica de roble.La temperatura adecuada para su degustacion es de 9º C. Es un vino apto para guarda de unos 2 años. Cuando el vino es joven, por su característica acidez, es perfecto para casi cualquier ensalada, o para platos con tomate, pimientos verdes,espárragos, o quesos como el feta o de cabra. También muy apropiado para el marisco o para pescados sabrosos. En general, el exceso de sal puede ser “apagado” por la acidez y la frescura de este vino. Para un aperitivo de anchoas en salazón y aceitunas, por ejemplo, un Torrontes puede ir a la perfección',
         variety: "Torrontes",
         year: 2021,
-        img:"images/products/img-bressia-sylvestra-torrontes.jpg"
+        img:"/images/products/img-bressia-sylvestra-torrontes.jpg"
 
     },
     {
@@ -55,7 +55,7 @@ let vinos = [
         description:'Vista: Tonos rojos de muy buena intensidad y matiz. De aspecto elegante y buena concentración Nariz: Muy buena expresión del varietal Merlot manifestando notas de trufas, guindas y moras como así también pimentosas y herbales potenciados por la presencia del Cabernet Franc. Acertado equilibrio entre la madera y la fruta. Boca: Su ingreso en boca es dulce, voluminoso y estructurado, revelando aromas de boca que recuerdan lo percibido en nariz.Armonía: Buen balance tánico que se manifesta con mucha elegancia y firmeza.',
         variety: "Merlot",
         year: 2012,
-        img:"images/products/img-bressia-del-alma-merlot.jpg"
+        img:"/images/products/img-bressia-del-alma-merlot.jpg"
 
     },
     {
@@ -65,7 +65,7 @@ let vinos = [
         description:'Esta es la primer cosecha de Catena Alta Malbec elaborada mediante el corte de microclimas: Las uvas de este vino, 100% Malbec, provienen de dos viñedos diferentes: el Lote 18 del Viñedo Angélica, de 70 años de edad, con suelo areno/arcilloso, y el Lote 3 del Viñedo Nicasia, de suelo arenoso. En ambos viñedos, la cosecha 2001 se caracterizó por temperaturas promedio levemente más cálidas durante el mes de marzo, con noches muy frescas, y con una amplitud térmica promedio más marcada de lo habitual. En el Viñedo Angélica, esto devino en fruta con intensos sabores de frutos negros y un paladar estructurado, pero suave a la vez, con taninos maduros y dulces. El Malbec del Viñedo Nicasia otorgó delicadas notas florales y dejos especiados de pimienta en el final de boca. Esta cosecha puede beberse y disfrutarse dentro de los próximos 4 a 5 años, continuando su evolución durante 10 años más.',
         variety: "Malbec",
         year: 2001,
-        img:"images/products/img-angelica-zapata-malbec.jpg"
+        img:"/images/products/img-angelica-zapata-malbec.jpg"
 
     },
     {
@@ -75,7 +75,7 @@ let vinos = [
         description:'Este Rosado de la Bodega Cielo y Tierra está realizado en los Viñedos ubicados en Perdriel, Luján de Cuyo a 950 msnm. Color rojo cereza con intenso halo rubí. En nariz, toque de frutilla confitada, mermelada ciruelas y un persistente cherry. En boca tiene un gran equilibrio azucar-acidez, con cuerpo untuoso y voluminoso, con un fresco frutado.',
         variety: "Canarí",
         year: 2021,
-        img:"images/products/img-callejon-de-las-brujas-canari.jpg"
+        img:"/images/products/img-callejon-de-las-brujas-canari.jpg"
 
     },
 ]
