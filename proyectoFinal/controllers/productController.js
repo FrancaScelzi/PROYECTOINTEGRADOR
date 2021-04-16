634c27e4af767e9d04dc04c65372011a66fb1a38
Franca Scelzi <69602652+FrancaScelzi@users.noreply.github.com>
2021-04-16 12:31:28 -0300

    Arme los comentarios y el perfil

--- proyectoFinal/controllers/productController.js
+++ proyectoFinal/controllers/productController.js
@@ -1,9 +1,9 @@
 let products = require('../data/productsData');
 
 let controller = {
-    show:(req,res)=>    res.render('product', { title: 'Productos de The Union Winery' , products: products , id: req.params.id}),
-    edit:(req,res)=>    res.render('product-add', {title: 'Editar Productos de The Union Winery', products: products}),
-    search:(req,res)=>   res.render('search-results', { title: 'Resultados de The Union Winery' , result: req.params.search}) ,
+    show:(req,res)=>    res.render('product', { title: 'Productos | The Union Winery' , products: products , id: req.params.id}),
+    edit:(req,res)=>    res.render('product-add', {title: 'Editar | The Union Winery', products: products}),
+    search:(req,res)=>   res.render('search-results', { title: 'Resultados | The Union Winery' , result: req.params.search}) ,
 
 } 
 
--- proyectoFinal/controllers/userController.js
+++ proyectoFinal/controllers/userController.js
@@ -1,11 +1,11 @@
 let products = require("../data/productsData")
 
 let controller = {
-    login:(req,res)=>   res.render('login', { title: 'Ingresa a The Union Winery' }),
-    register:(req,res)=>   res.render('register', { title: 'Registrarse a The Union Winery' }),
-    profile:(req,res)=>   res.render('profile', { title: 'Tu perfil de The Union Winery', productos:products }),
-    edit:(req,res)=> res.render('profile-edit', {title:'Edita Tu Perfil de The Union Winery'}),
-    logout:(req,res)=>   res.render('logout', { title: 'Logout de The Union Winery' }) 
+    login:(req,res)=>   res.render('login', { title: 'Ingresá | The Union Winery' }),
+    register:(req,res)=>   res.render('register', { title: 'Registrate | The Union Winery' }),
+    profile:(req,res)=>   res.render('profile', { title: 'Perfil | The Union Winery', productos:products }),
+    edit:(req,res)=> res.render('profile-edit', {title:'Editar Perfil | The Union Winery'}),
+    logout:(req,res)=>   res.render('logout', { title: 'Logout | The Union Winery' }) 
 
 } 
 
