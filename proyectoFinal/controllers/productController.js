62193ad38a9fee8fd210a97ffe9a0cabeadcfc22
MiltonKienigiel <miltonkienigiel@gmail.com>
2021-04-19 11:57:56 -0300

    Cambios en el header, search results armado.

--- proyectoFinal/controllers/productController.js
+++ proyectoFinal/controllers/productController.js
@@ -3,7 +3,7 @@ let products = require('../data/productsData');
 let controller = {
     show:(req,res)=>    res.render('product', { title: 'Productos | The Union Winery' , products: products , id: req.params.id}),
     edit:(req,res)=>    res.render('product-add', {title: 'Editar | The Union Winery', products: products}),
-    search:(req,res)=>   res.render('searchResults', { title: 'Resultados | The Union Winery' , result: req.query.search}) ,
+    search:(req,res)=>   res.render('searchResults', { title: 'Resultados | The Union Winery' , products: products , result: req.query.search}) ,
 
 } 
 
