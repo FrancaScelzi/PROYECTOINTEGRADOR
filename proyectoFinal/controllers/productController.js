93abdd5c2779c76ce932893ef42196ef50d5ba38
MiltonKienigiel <miltonkienigiel@gmail.com>
2021-04-24 12:38:02 -0300

    Minor bugs

--- proyectoFinal/controllers/productController.js
+++ proyectoFinal/controllers/productController.js
@@ -3,7 +3,7 @@ let users = require('../data/usersData');
 
 let controller = {
     show:(req,res)=>    res.render('product', { title: 'Productos | The Union Winery' , products: products , id: req.params.id , users:users}),
-    edit:(req,res)=>    res.render('product-add', {title: 'Editar | The Union Winery', products: products}),
+    edit:(req,res)=>    res.render('product-add', {title: 'Editar | The Union Winery', products: products, id: req.params.id , users:users}),
     search:(req,res)=>  res.render('searchResults', { title: 'Resultados | The Union Winery' , products: products , result: req.query.search}) ,
 
 } 
