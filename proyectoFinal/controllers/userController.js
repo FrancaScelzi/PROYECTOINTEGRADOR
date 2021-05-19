cd35246454483b03175ecbf771dd4f898467333f
MiltonKienigiel <miltonkienigiel@gmail.com>
2021-05-19 13:05:09 -0300

    Register funcional
    Los usuarios creados se guardan en la db. Se guarda la session del login pero estamos con un tema ahi que no redirige el login

--- proyectoFinal/controllers/userController.js
+++ proyectoFinal/controllers/userController.js
@@ -6,8 +6,6 @@ const op = db.Sequelize.Op
 
 let users = require("../data/usersData")
 let controller = {
-    login:(req,res)=>   res.render('login', { title: 'Ingresá | The Union Winery' }),
-    register:(req,res)=>   res.render('register', { title: 'Registrate | The Union Winery' }),
     profile:(req,res)=>   res.render('profile', { title: 'Perfil | The Union Winery', productos:products, id:req.params.id , users:users }),
     edit:(req,res)=> res.render('profile-edit', {title:'Editar Perfil | The Union Winery',  id:req.params.id , users:users}),
     logout:(req,res)=>   res.render('logout', { title: 'Logout | The Union Winery' }),
--- proyectoFinal/routes/login.js
+++ proyectoFinal/routes/login.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 let loginController = require('../controllers/loginController')
-/* GET users listing. */
-
 
 router.get('/', loginController.index);
-router.post('/', loginController.store);
+router.post('/', loginController.login);
+router.post('/logout', loginController.logout);
+
 
 module.exports = router;
